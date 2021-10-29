b6f7ef80690222c0cadaba718818be6d99ea52bd
behnam <behnam74gh@yahoo.com>
2021-10-29 16:41:35 +0330

    layout-completed-commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
 import React from "react";
-import { BrowserRouter, Switch, Link } from "react-router-dom";
-// import {} from 'antd'
+import { Switch, Link, Route } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import {
+  Navbar,
+  HomePage,
+  Exchanges,
+  Cryptocurrencies,
+  CryptoDetails,
+  News,
+} from "./components";
 import "./App.css";
 
 const App = () => {
   return (
-    <div>
-      <h2>hello crypto</h2>
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout style={{ minHeight: "85vh" }}>
+          <div className="routes">
+            <Switch>
+              <Route exact path="/" component={HomePage} />
+              <Route exact path="/exchanges" component={Exchanges} />
+              <Route
+                exact
+                path="/cryptocurrencies"
+                component={Cryptocurrencies}
+              />
+              <Route exact path="/crypto/:coinId" component={CryptoDetails} />
+              <Route exact path="/news" component={News} />
+              {/*<Route exact component={NotFound} />*/}
+            </Switch>
+          </div>
+        </Layout>
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            Cryptoverse <br />
+            All Rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
+      </div>
     </div>
   );
 };
