c700737a77293c5e6eeacf6866f94378df1dbbe0
behnam <behnam74gh@yahoo.com>
2021-10-31 15:28:55 +0330

    cryptocurrenciesPage-comitt

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,10 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import millify from "millify";
+import { Link } from "react-router-dom";
+import { Card, Row, Col, Input } from "antd";
 
-const Cryptocurrencies = () => {
-  return (
-    <div>
-      <h1>Cryptocurrencies</h1>
-    </div>
+import { useGetCryptosQuery } from "../services/cryptoApi";
+
+const Cryptocurrencies = ({ simplified }) => {
+  const count = simplified ? 10 : 100;
+  const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
+  const [cryptos, setCryptos] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    if (cryptosList && cryptosList.data && cryptosList.data.coins) {
+      setCryptos(cryptosList.data.coins);
+    }
+  }, [cryptosList]);
+
+  const searched = (searchTerm) => (coin) =>
+    coin.name.toLowerCase().includes(searchTerm.toLowerCase());
+
+  return isFetching ? (
+    "Loading.."
+  ) : (
+    <>
+      {!simplified && (
+        <div className="search-crypto">
+          <Input
+            placeholder="Search Cryptocurrency"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+      )}
+      <Row gutter={[32, 32]} className="crypto-card-container">
+        {cryptos.filter(searched(searchTerm)).length > 0 &&
+          cryptos.filter(searched(searchTerm)).map((currency) => (
+            <Col
+              xs={24}
+              sm={12}
+              lg={6}
+              className="crypto-card"
+              key={currency.id}
+            >
+              <Link to={`/crypto/${currency.id}`}>
+                <Card
+                  title={`${currency.rank}. ${currency.name}`}
+                  extra={
+                    <img
+                      src={currency.iconUrl}
+                      alt={currency.name}
+                      className="crypto-image"
+                    />
+                  }
+                  hoverable
+                >
+                  <p>Price: {millify(currency.price)}</p>
+                  <p>Market Cap: {millify(currency.marketCap)}</p>
+                  <p>Daily Change: {millify(currency.change)}</p>
+                </Card>
+              </Link>
+            </Col>
+          ))}
+      </Row>
+    </>
   );
 };
 
